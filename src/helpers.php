3f16a69c4101a6cebeb93776571ff7317baf2518
Loduis Madariaga <loduis@gmail.com>
2024-07-20 12:07:04 -0500

    feat: add person and regime.

--- src/helpers.php
+++ src/helpers.php
@@ -18,6 +18,18 @@ const DOC_DEBIT_NOTE = 6;
 const DOC_REMISSION_NOTE = 7;
 const DOC_WITHHOLDING = 8;
 
+const PERSON_NATURAL = 1;
+const PERSON_LEGAL = 2;
+
+const REGIME_TOURISM = 1;
+const REGIME_IMPORTER = 2;
+const REGIME_EXPORTER = 3;
+const REGIME_MAQUILA = 4;
+const REGIME_LAW_6090 = 5;
+const REGIME_SMALL_PRODUCER = 6;
+const REGIME_MEDIUM_PRODUCER = 7;
+const REGIME_ACCOUNTING = 8;
+
 const EMISSION_TYPES = [
     EMISSION_NORMAL => 'Normal',
     EMISSION_CONTINGENCY => 'Contingencia'
@@ -32,6 +44,17 @@ const DOC_TYPES = [
     DOC_DEBIT_NOTE => 'Nota de débito electrónica',
     DOC_REMISSION_NOTE => 'Nota de remisión electrónica',
     DOC_WITHHOLDING => 'Comprobante de retención electrónico'
+];
+
+const REGIME_TYPES = [
+    REGIME_TOURISM => 'Régimen de Turismo',
+    REGIME_IMPORTER => 'Importador',
+    REGIME_EXPORTER => 'Exportador',
+    REGIME_MAQUILA => 'Maquila',
+    REGIME_LAW_6090 => 'Ley N° 60/90',
+    REGIME_SMALL_PRODUCER => 'Régimen del Pequeño Productor',
+    REGIME_MEDIUM_PRODUCER => 'Régimen del Mediano Productor',
+    REGIME_ACCOUNTING => 'Régimen Contable',
 ];
 
 function get_key(...$entries): string
